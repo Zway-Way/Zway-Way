52965766c3757068b122a9e782baf9879cec370f
Raymond <zhur@wsc.school.nz>
2021-05-17 12:33:06 +1200

    sample

---
+++ Assets/Player Movement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
+    // Update is called once per frame
+    void Update()
+    {
+        // move the ball forward
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        //moves the car forward on vertical axis
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //rotates the car on horizontal axis
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
+
+    }
+}
