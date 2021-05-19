a994f60e2db59a7c86fc7239d1e4ce6f2888dd99
Raymond <zhur@wsc.school.nz>
2021-05-19 14:47:40 +1200

    Actual Progress

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private float speed = 20f;
+    private float turnSpeed = 15f;
+    private float horizontalInput;
+    private float forwardInput;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // move the ball forward
+        horizontalInput = Input.GetAxis("Horizontal");
+        //forwardInput = Input.GetAxis("Vertical");
+        //make use of the vertical code to add a jump mechanic
+        //moves the ball forward on vertical axis
+        //transform.Rotate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //rotates the ball on horizontal axis
+        transform.Translate(Vector3.left * Time.deltaTime * turnSpeed * horizontalInput);
+
+    }
+}
+
+ 
--- Assets/Scripts/CameraTracking.cs
+++ Assets/Scripts/CameraTracking.cs
@@ -4,36 +4,39 @@ using UnityEngine;
 
 public class CameraTracking : MonoBehaviour
 {
+    public GameObject Ball;
     // Start is called before the first frame update
     void Start()
     {
         
     }
+    private Vector3 offset = new Vector3();
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Ball.transform.position + offset;
     }
 }
 
 
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-public class FollowPlayer : MonoBehaviour
-{
-    public GameObject Player;
-    // Start is called before the first frame update
-    void Start()
-    {
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//public class FollowPlayer : MonoBehaviour
+//{
+//    public GameObject Player;
+//    // Start is called before the first frame update
+//    void Start()
+//    {
 
-    }
-    private Vector3 offset = new Vector3(0, 5, -7);
-    // Update is called once per frame
-    void Update()
-    {
-        // update the camerea to be behind the players vehicle
-        transform.position = Player.transform.position + offset;
-    }
-}
+//    }
+//    private Vector3 offset = new Vector3(0, 5, -7);
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        // update the camerea to be behind the players vehicle
+//        transform.position = Player.transform.position + offset;
+//    }
+//}
